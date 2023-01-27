73b9ddf3bf69a8a494b39754d32009464a2d12ce
matias de la cerda <mdelacerda@ecomsur.com>
2023-01-27 15:35:27 -0300

    cambios en el modelo de usuario

--- src/resources/users/models/users.model.js
+++ src/resources/users/models/users.model.js
@@ -1,42 +1,37 @@
-import bcrypt from 'bcrypt'
-import mongoose from 'mongoose'
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
-const SALT_WORK_FACTOR = 12
-const userSchema = new mongoose.Schema( {
-  name: {
+const SALT_WORK_FACTOR = 12;
+const userSchema = new mongoose.Schema({
+  usuario: {
     type: String,
-    required: true
-  },
-  surname: {
-    type: String,
-    required: true
+    require: true,
   },
   email: {
     type: String,
-    required: true,
-    index: { unique: true }
+    require: true,
   },
   password: {
     type: String,
-    required: true
-  }
-} )
+    require: true,
+  },
+});
 
-userSchema.pre( 'save', async function(next ){
-  if ( !this.isModified( 'password' ) ) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   try {
-    const salt = await bcrypt.genSalt( SALT_WORK_FACTOR );
-    this.password = await bcrypt.hash( this.password, salt );
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
     return next();
-
-  } catch ( err ) {
-    return next( err );
+  } catch (err) {
+    return next(err);
   }
-} )
+});
 
-userSchema.methods.validatePassword = async function validatePassword( password ) {
-  return bcrypt.compare( password, this.password );
+userSchema.methods.validatePassword = async function validatePassword(
+  password
+) {
+  return bcrypt.compare(password, this.password);
 };
 
-
-export const UserModel = new mongoose.model( 'User', userSchema ) 
+export const UserModel = new mongoose.model("User", userSchema);
