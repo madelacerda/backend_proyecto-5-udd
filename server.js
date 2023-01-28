3c26d419b1a8e4be1f280ec20227e832b3397a9b
madelacerda <matiasdlc95.mdlc@gmail.com>
2023-01-27 23:45:23 -0300

    ultimos arreglos para el backend

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import seedRouter from "./routes/seedRoutes.js";
+
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import orderRouter from "./routes/orderRoutes.js";
-import uploadRouter from "./routes/uploadRoutes.js";
+
 import cors from "cors";
 
 dotenv.config();
@@ -29,11 +28,9 @@ app.get("/api/keys/paypal", (req, res) => {
 });
 
 
-app.use("/api/upload", uploadRouter);
-app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
+
 
 
 
